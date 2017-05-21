4dd8f8114a3c1e89273487777880f017831f9108
Tom Donoghue <tdonoghue@ucsd.edu>
2017-05-21 13:13:17 -0700

    Finish add teams script

--- mk_teams.py
+++ mk_teams.py
@@ -1,11 +1,12 @@
 """Create teams of project groups on Github."""
 
 import json
+from auth import *
 
 ##########################################################################################
 ##########################################################################################
 
-IDS_FILE = ''
+GIT_IDS_FILE = ''
 GROUPS_FILE = ''
 
 ##########################################################################################
@@ -14,13 +15,20 @@ GROUPS_FILE = ''
 def main():
     """   """
 
+    # Set up params for Github API
+    auth = 'Authorization: token ' + TOKEN
+    teams_url = 'https://api.github.com/orgs/COGS108/teams'
+
     # Load project groups
     with open(GROUPS_FILE, 'r') as fp:
         groups = json.load(fp)
 
     # Load github account names
-    with open(IDS_FILE, 'r') as fp:
+    with open(GIT_IDS_FILE, 'r') as fp:
         github_accts = json.load(fp)
+
+    # Initialize dictionary to save team IDs
+    teams_ids = dict()
 
     # Loop through groups
     for group, members in groups.items():
@@ -33,8 +41,33 @@ def main():
             except KeyError:
                 print('No ID for', member)
 
+        # Set up description for team
+        team_params = '{"name":"' + group + \
+                      '", "description":"Project team for ' + group + '"}'
+
         # Create Github team
-        #TODO
+        out = subprocess.run(['curl', '-XPOST', '-H', auth, teams_url,
+                             '-d', team_params], stdout=subprocess.PIPE)
+
+        # Get new team Github id, add to dictionary
+        team_id = json.loads(qq.stdout)['id']
+        teams_ids[group] = team_id
+
+        # Add members to team
+        for member in members:
+
+            # Built the URL for current student and team
+            mem_url = 'https://api.github.com/teams/' + team_id + \
+                      '/memberships/' + github_accts[member]
+
+            # Add the current student to github team
+            out = subprocess.run(['curl', '-XPUT', '-i', '-H', auth,  mem_url],
+                                 stdout=subprocess.PIPE)
+
+    # Save out team ids
+    with open('team_ids.json', 'w') as fp:
+        json.dump(team_ids, fp, sort_keys=True, indent=2)
+
 
 if __name__ == "__main__":
     main()
