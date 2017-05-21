a7aef82ca9105e68cf8384a77e9e7a5e5be8a490
Tom Donoghue <tdonoghue@ucsd.edu>
2017-05-21 13:13:03 -0700

    Finish add repo script

--- mk_repos.py
+++ mk_repos.py
@@ -1,8 +1,16 @@
 """Create repos for project groups on Github."""
 
+import json
+import base64
+from auth import *
+
 ##########################################################################################
 ##########################################################################################
 
+TEAM_IDS_FILE = ''
+
+README_CONTENT = ''
+README_CONTENT = base64.b64encode(README_CONTENT)
 
 ##########################################################################################
 ##########################################################################################
@@ -11,14 +19,43 @@
 def main():
     """   """
 
-    # Get list of teams on COGS108
-    #TODO
+    # Set up params for Github API
+    auth = 'Authorization: token ' + TOKEN
+    repos_url = 'https://api.github.com/orgs/COGS108/repos'
 
-    # Make repo
-    #TODO
+    # Load project team IDs
+    with open(TEAM_IDS_FILE, 'r') as fp:
+        teams = json.load(fp)
 
-    # Add README file to group project repo
-    #TODO
+    # Loop through teams
+    for group, team_id in teams.items():
+
+        # Init repository name
+        repo_name = 'Pr_' + group[-3]
+
+        # Set up repository information
+        repo_dat = '{"name":"' + repo_name + '",' + \
+                    '"description":"Project repo for ' + group + '",'+ \
+                    '"private":true,' + \
+                    '"team_id":' + team_id + '}'
+
+        # Create repository on COGS108
+        out = subprocess.run(['curl', '-XPOST', '-H', auth, repos_url, '-d', repo_dat],
+                             stdout=subprocess.PIPE)
+
+        # Initialzie URL to add a README file
+        readme_url = 'https://api.github.com/repos/COGS108/' + \
+                     repo_name + '/contents/README.md'
+
+        # Set up README data
+        readme_dat = '{"path": "README.md",' + \
+                     '"message":"Add README to project repo.",' + \
+                     '"committer":{"name":"Tom Donoghue", "email":"tdonoghue@ucsd.edu"},' + \
+                     '"content":"' + README_CONTENT + '"}'
+
+        # Add README file to group project repo
+        subprocess.run(['curl', '-i', '-X', 'PUT', '-H', auth, '-d', repo_dat, repo_url],
+                       stdout=subprocess.PIPE)
 
 
 if __name__ == "__main__":
